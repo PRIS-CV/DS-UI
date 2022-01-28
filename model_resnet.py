f1e2ed641f767399eb6ccfafdbed26a3c334d751
luminolx <50606356+luminolx@users.noreply.github.com>
2022-01-28 11:31:52 +0800

    Update model_resnet.py

--- model_resnet.py
+++ model_resnet.py
@@ -6,8 +6,7 @@ from torch.nn import init
 
 def conv3x3(in_planes, out_planes, stride=1):
     "3x3 convolution with padding"
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=False)
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
 
 class BasicBlock(nn.Module):
     expansion = 1
